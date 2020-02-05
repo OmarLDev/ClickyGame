ac101d7827740040a8524080b3e26ae4b7a7424f
Omar Luna <mrebel62@gmail.com>
2020-02-04 20:36:46 -0600

    Updates

---
+++ precache-manifest.86968eb6fea22e02acc46b84f39d7b97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bbf077ce1390df0e9dda8395e0fe7b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "85ce1c5e8e224e2a8a1d",
+    "url": "/ClickyGame/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "73720c2b7d18a1c9e0af",
+    "url": "/ClickyGame/static/js/2.eab0faef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ClickyGame/static/js/2.eab0faef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ce1c5e8e224e2a8a1d",
+    "url": "/ClickyGame/static/js/main.d9a9252f.chunk.js"
+  },
+  {
+    "revision": "823aaea0a44c1006039f",
+    "url": "/ClickyGame/static/js/runtime-main.67b525c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
