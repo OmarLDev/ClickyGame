7c2a7f3947246d933810f8b3238325d35bc6fe23
Omar Luna <mrebel62@gmail.com>
2020-02-13 15:52:26 -0600

    loss functionality

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Jumbotron from './components/Jumbotron';
@@ -10,15 +9,42 @@ import characters from './characters.json';
 
 class App extends React.Component {
 
+  // Initial state
   state ={
     score: 0,
     topScore: 0,
-    clicked: []
+    clicked: [],
+    characters: characters
   }
 
+  // Function to handle when a user has clicked all images correctly
+  handleWin = () =>{
+
+  }
+
+  // Function to handle when a user clicked twice on an element
+  handleLoss =() =>{
+    this.setState({
+      score: 0,
+      clicked: []
+    });
+  }
+
+  // Event to handle if an image has been clicked twice
   handleCharacterClick = (id) => {
-    console.log(id);
-    this.setState({score: this.state.score + 1});
+    if(!this.state.clicked.includes(id)){
+      this.setState(state => {
+        const newArray = state.clicked.concat(id);
+        if(this.state.score === this.state.topScore){
+          this.setState({topScore: this.state.topScore +1})
+        }
+        return{clicked: newArray,
+               score: this.state.score + 1
+        }
+      });
+    }else{
+      this.handleLoss();
+    }
   }
 
   render() {
@@ -28,6 +54,7 @@ class App extends React.Component {
       < Jumbotron/>
         < Wrapper>
         {
+          // Iterating the character list to display
           characters.map( element => 
             < Card image={element.image} 
                     name={element.name} 
